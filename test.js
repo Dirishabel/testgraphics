279a33b9480ef03c01f9749a467169144324bf0f
Dmitryy Artemjev <artemjev42@gmail.com>
2020-12-02 17:16:39 +0500

    change imports

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
+import MG from 'D:\\metrics-graphics'
+
 d3.json('data/fake_users1.json', function(data) {
-    data = MG.convert.date(data, 'date');
-    MG.data_graphic({
-        title: "Line Chart",
-        description: "This is a simple line chart. You can remove the area portion by adding area: false to the arguments list.",
-        data: data,
-        width: 600,
-        height: 200,
-        right: 40,
-        target: document.getElementById('fake_users1'),
-        x_accessor: 'date',
-        y_accessor: 'value'
-    });
+  data = MG.convert.date(data, 'date');
+  MG.data_graphic({
+      title: "Line Chart",
+      description: "This is a simple line chart. You can remove the area portion by adding area: false to the arguments list.",
+      data: data,
+      width: 600,
+      height: 200,
+      right: 40,
+      target: document.getElementById('fake_users1'),
+      x_accessor: 'date',
+      y_accessor: 'value'
+  });
 });
